788dfb45248ee1ff14c86936dc0a132a3789196b
AlejaRairan <alejandra.0219@hotmail.com>
2026-09-11 17:22:12 -0500

    Agregar ruta temporal /crash-test

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('El servidor va a caer');
+  }, 0);
+});
 app.post("/signin", validateLogin, login);
 app.post("/signup", validateCreateUser, createUser);
 app.use(auth);
